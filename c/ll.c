440cdeb8904889dacbb72c2d079c85e9f6f199f9
Andrew Jarrett <jarrett.andrew@gmail.com>
2024-03-01 14:06:57 -0500

    Add WIP linked list method

--- c/ll.c
+++ c/ll.c
@@ -12,10 +12,51 @@ void insert_first(List list, Node node)
 	}
 }
 
+void remove_first(List list, Node node)
+{
+	if (list.first != NULL)
+	{
+		list.first = list.first->next;
+	}
+	else
+	{
+		list.first = NULL;
+	}
+}
+
 void insert_after(Node node, Node newNode)
 {
 	newNode.next = node.next;
 	node.next = &newNode;
+}
+
+void remove_after(List list, Node node)
+{
+	Node *toRemove = node.next;
+	if (toRemove != NULL)
+	{
+		Node *nextNode = toRemove->next;
+		node.next = nextNode;
+		update_last(list, *toRemove, *nextNode);
+	}
+	else
+	{
+		node.next = NULL;
+	}
+}
+
+void update_last(List list, Node prevNode, Node newNode)
+{
+	// Check to see if the prev node is the last node, and if so update
+	// it to be the new node
+	if (list.last == &prevNode)
+	{
+		list.last = &newNode;
+	}
+	else
+	{
+		list.last = NULL;
+	}
 }
 
 void ll_test(int num_nodes)
--- c/ll.h
+++ c/ll.h
@@ -13,5 +13,10 @@ typedef struct List {
 } List;
 
 void insert_first(List list, Node node);
+void remove_first(List list, Node node);
 void insert_after(Node node, Node newNode);
+void remove_after(List list, Node node);
+
+void update_last(List list, Node prevNode, Node newNode);
+
 void ll_test(int num_nodes);
