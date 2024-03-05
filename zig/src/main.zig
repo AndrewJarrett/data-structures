15ab3c39e7aa3670de71ded22e9ff7465baf99ab
Andrew Jarrett <jarrett.andrew@gmail.com>
2024-03-05 00:09:57 -0500

    Do init-lib and move files

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
+const testing = std.testing;
 
 pub fn main() void {
     std.debug.print("Welcome to data structure in Zig. Let's have fun!");
 }
+
+// Main tests go here
 
